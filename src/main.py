fa55b8901a4b36448dc2f4f39d4c2f86cc49f897
Priji Balakrishnan <prijibalakrishnan@gmail.com>
2019-01-06 13:27:32 +0800

    Clean main file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import lib
 
-# #To generate a hemispherical tree crown. Use this shape when the crown's height and radius are similar to each other.
+#To generate a hemispherical tree crown. Use this shape when the crown's height and radius are similar to each other.
 lib.genHemisphere(vertices_count=20000, gapPercentage=0.136, width=5.0)
-#
-# #To generate a truncated prolate ellipsoid tree crown. Use this shape when the crown's height > radius
+
+#To generate a truncated prolate ellipsoid tree crown. Use this shape when the crown's height > radius
 lib.genEllipsoidProlate(vertices_count=20000, gapPercentage=0.059, height=7.8, radius=5.35)
 
 #To generate a truncated oblate ellipsoid tree crown. Use this shape when the crown's height < radius
